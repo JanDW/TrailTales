0e504e6926e47fcf71199730623ca7ece09138ae
Jan De Wilde <jan@jandewilde.com>
2024-11-11 17:30:38 -0500

    Fixed shared data between walks

--- src/_content/walks/walks.11tydata.js
+++ src/_content/walks/walks.11tydata.js
@@ -11,5 +11,24 @@ export default (data) => ({
   layout: 'walks.webc',
   eleventyComputed: {
     permalink: (data) => getPermalink(data),
+    gpxData: (data) => {
+      const test = data.gpx.filter(
+        (gpxItem) => gpxItem.fileName === data.gpxFile
+      )[0];
+      return test;
+    },
+    shots: (data) => {
+      console.log(data.images[data.photos[0].src].exif.Caption);
+      data.photos.map((photo) => {
+        if (photo) {
+          photo.lat = data.images[photo.src]?.exif.latitude;
+          photo.lon = data.images[photo.src]?.exif.longitude;
+          photo.caption = data.images[photo.src]?.exif.Caption;
+          photo.dateTime = data.images[photo.src]?.exif.DateTimeOriginal;
+          photo.lens = data.images[photo.src]?.exif.LensModel;
+        }
+        return photo;
+      });
+    },
   },
 });
