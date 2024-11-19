d8d051b920010d1154d7dfc3c1d1baf123e8374d
Jan De Wilde <jan@jandewilde.com>
2024-11-18 22:19:31 -0500

    Minor tweaks

--- node/exif.cjs
+++ node/exif.cjs
@@ -2,7 +2,7 @@ const exifr = require('exifr');
 
 async function readExif() {
   const exif = await exifr.parse(
-    '../src/_data/images/11-09-24-fitzgerald-lake-sofie - 4.jpeg',
+    '../src/_data/images/11-17-24-buffam-falls - 6.jpeg',
     true
   );
   console.log(exif);
